c471c3d760fab4486c1ccb8eeb6f43bdbdba612b
miten-cl <miten@codelambda.com>
2025-08-22 15:12:48 +0530

    update role middleware

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -3,18 +3,20 @@ const errorResponse = require("../helper/response");
 const { ERROR_MESSAGES, REQUIRED_ROLES } = require("../config/constants.js");
 const isAuthorized = require("../helper/utils.js");
 
-const roleMiddleware = async (
-  req,
-  res,
-  next
-) => {
-  const {user} = req;
-  let accountRoles = user.role;
-  if (!isAuthorized(accountRoles, REQUIRED_ROLES)) {
-    return errorResponse(res, 403, ERROR_MESSAGES.FORBIDDEN);
-  }
+const roleMiddleware = (allowedRoles = []) => {
+  return async (req, res, next) => {
+    const { user } = req;
+    let accountRoles = user.role;
+    
+    // If no specific roles provided, use default REQUIRED_ROLES from constants
+    const rolesToCheck = allowedRoles.length > 0 ? allowedRoles : REQUIRED_ROLES;
+    
+    if (!isAuthorized(accountRoles, rolesToCheck)) {
+      return errorResponse(res, 403, ERROR_MESSAGES.FORBIDDEN);
+    }
 
-  return next();
+    return next();
+  };
 };
 
 module.exports = roleMiddleware;
